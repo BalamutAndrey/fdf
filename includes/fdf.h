b6b2b1615739cca6a5c34d884ed79eac670f872a
Gall Eliz <geliz@ga-c3.msk.21-school.ru>
2020-11-14 16:36:40 +0300

    norme .h

--- includes/fdf.h
+++ includes/fdf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eboris <eboris@student.42.fr>              +#+  +:+       +#+        */
+/*   By: geliz <geliz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 15:46:00 by eboris            #+#    #+#             */
-/*   Updated: 2020/11/14 16:24:25 by eboris           ###   ########.fr       */
+/*   Updated: 2020/11/14 16:32:09 by geliz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,43 @@
 # define SUCCESS 1
 # define END 0
 
-typedef	struct	s_color
+typedef	struct		s_color
 {
-	char	red;
-	char	green;
-	char	blue;
-	char	alpha;
-}				t_color;
+	char			red;
+	char			green;
+	char			blue;
+	char			alpha;
+}					t_color;
 
-
-typedef struct	s_bres
+typedef struct		s_bres
 {
-	int		x1;
-	int		y1;
-	int		x2;
-	int		y2;
-	int		z1;
-	int		z2;
-}				t_bres;
+	int				x1;
+	int				y1;
+	int				x2;
+	int				y2;
+	int				z1;
+	int				z2;
+}					t_bres;
 
-typedef struct	s_fdf
+typedef struct		s_fdf
 {
-	void		*mlx;
-	void		*win;
-	void		*img;
-	void		*img_iso;
-	bool		image;
-	char		*data;
-	char		*data_iso;
-	char		*data_cls;
-	int			bit;
-	int			size;
-	int			endian;
-	int			map_x;
-	int			map_y;
-	int			**map;
-	int			zoom;
-//	int			map[11][19];
-	t_bres		*bres;
-}				t_fdf;
+	void			*mlx;
+	void			*win;
+	void			*img;
+	void			*img_iso;
+	bool			image;
+	char			*data;
+	char			*data_iso;
+	char			*data_cls;
+	int				bit;
+	int				size;
+	int				endian;
+	int				map_x;
+	int				map_y;
+	int				**map;
+	int				zoom;
+	t_bres			*bres;
+}					t_fdf;
 
 typedef struct		s_str
 {
@@ -80,79 +78,90 @@ typedef struct		s_str
 /*
 ** fdf.c
 */
-int				main(int argc, char **argv);
-int		fdf_close(void *param);
-void			fdf_exit(t_fdf *fdf);
+int					main(int argc, char **argv);
+int					fdf_close(void *param);
+void				fdf_exit(t_fdf *fdf);
 /*
 ** fdf_struct.c
 */
-t_fdf			*fdf_create_fdf(void);
-void			fdf_remove_fdf(t_fdf **fdf);
+t_fdf				*fdf_create_fdf(void);
+void				fdf_remove_fdf(t_fdf **fdf);
 /*
 ** fdf_func.c
 */
-void	*fdf_memalloc(t_fdf *fdf, size_t size);
+void				*fdf_memalloc(t_fdf *fdf, size_t size);
 /*
 ** fdf_error.c
 */
-void	fdf_error(t_fdf *fdf, char *err);
+void				fdf_error(t_fdf *fdf, char *err);
 /*
 ** fdf_readmap.c
 */
-void	fdf_readmap(t_fdf *fdf, char *arg);
-void	fdf_readmap_check(t_fdf *fdf, char *str, t_str *first);
-void	fdf_read_clean(char *str, t_str *fir, int fir_fl);
-void	fdf_fillmap(t_fdf *fdf, t_str *first);
+void				fdf_readmap(t_fdf *fdf, char *arg);
+void				fdf_readmap_check(t_fdf *fdf, char *str, t_str *first);
+void				fdf_read_clean(char *str, t_str *fir, int fir_fl);
+void				fdf_fillmap(t_fdf *fdf, t_str *first);
 /*
-** 
+** fdf_map_valid.c
 */
-int		fdf_string_to_arr(t_fdf *fdf, t_str *first, char *str);
-void	fdf_fill_str_struct(t_fdf *fdf, t_str *first, char **split, int i);
-void	fdf_fill_arr(t_fdf *fdf, t_str *f, char **split, int len);
-int		fdf_check_is_number(t_fdf *fdf, char *str, char **spl);
-void	fdf_remove_split(char ***split);
+int					fdf_string_to_arr(t_fdf *fdf, t_str *first, char *str);
+void				fdf_fill_str_struct(t_fdf *fdf, t_str *first, char **split,
+						int i);
+void				fdf_fill_arr(t_fdf *fdf, t_str *f, char **split, int len);
+int					fdf_check_is_number(t_fdf *fdf, char *str, char **spl);
+void				fdf_remove_split(char ***split);
 /*
 ** fdf_init.c
 */
-void    fdf_init(t_fdf *fdf);
+void				fdf_init(t_fdf *fdf);
 /*
 ** fdf_keyboard.c
 */
-int		fdf_keyboard(int key, void *param);
+int					fdf_keyboard(int key, void *param);
 /*
 ** fdf_draw.c
 */
-void	fdf_draw(t_fdf *fdf);
+void				fdf_draw(t_fdf *fdf);
 /*
 ** fdf_math.c
 */
-void	fdf_math(t_fdf *fdf);
-void    fdf_math_x(t_fdf *fdf, int x, int y);
-void    fdf_math_y(t_fdf *fdf, int x, int y);
+void				fdf_math(t_fdf *fdf);
+void				fdf_math_x(t_fdf *fdf, int x, int y);
+void				fdf_math_y(t_fdf *fdf, int x, int y);
 /*
 ** fdf_math_parallel.c
 */
-void	fdf_math_parallel(t_fdf *fdf);
-void	fdf_math_parallel_write(t_fdf *fdf, t_color color);
-void	fdf_put_pixel_paralel(t_fdf *fdf, int x, int y, t_color color);
+void				fdf_math_parallel(t_fdf *fdf);
+void				fdf_math_parallel_write(t_fdf *fdf, t_color color);
+void				fdf_put_pixel_paralel(t_fdf *fdf, int x, int y,
+						t_color color);
 /*
 ** fdf_math_iso.c
 */
-void	fdf_math_iso(t_fdf *fdf);
-void	fdf_math_iso_rotate(t_fdf *fdf);
-void	fdf_math_iso_write(t_fdf *fdf, t_color color);
-void	fdf_math_iso_write_sign(t_fdf *fdf, double *sign_x, double *sign_y);
-void	fdf_put_pixel_iso(t_fdf *fdf, int x, int y, t_color color);
+void				fdf_math_iso(t_fdf *fdf);
+void				fdf_math_iso_rotate(t_fdf *fdf);
+void				fdf_math_iso_write(t_fdf *fdf, t_color color);
+void				fdf_math_iso_write_sign(t_fdf *fdf, double *sign_x,
+						double *sign_y);
+void				fdf_put_pixel_iso(t_fdf *fdf, int x, int y, t_color color);
 /*
 ** fdf_math_iso_rotate.c
 */
-void	fdf_rotate_x(int *y, int *z, double alpha);
-void	fdf_rotate_y(int *x, int *z, double beta);
-void	fdf_rotate_z(int *x, int *y, double gamma);
-
-
-int		get_next_line(const int fd, char **line);
-
-void	ft_lst_del(t_list *first);
+void				fdf_rotate_x(int *y, int *z, double alpha);
+void				fdf_rotate_y(int *x, int *z, double beta);
+void				fdf_rotate_z(int *x, int *y, double gamma);
+/*
+** gnl.c
+*/
+int					get_next_line(const int fd, char **line);
+static t_list		*ft_findlist(t_list *first, int fd);
+static int			ft_readplus(t_list *temp, char **line, char *buf);
+static int			ft_fillstring(t_list *temp, char **line, char *part,
+						int res);
+static char			*ft_giveline(t_list *temp, char *part);
+/*
+** gnl_clear.c
+*/
+void				ft_lst_del(t_list *first);
 
 #endif
